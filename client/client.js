4d5afbb40a4d7fcec82c414ea0b00870d8a1cfc9
Arthur Maroulier <git@maroulier.fr>
2015-11-18 21:32:03 +0100

    Add votes counts in the buttons.

--- client/client.js
+++ client/client.js
@@ -14,3 +14,26 @@ Template.button.helpers({
     return 'btn ' + str.replace(/[^a-z0-9]/ig, '').toLowerCase();
   }
 });
+
+Template.button.helpers({
+  getButtonClass: function (str) {
+    return 'btn ' + str.replace(/[^a-z0-9]/ig, '').toLowerCase();
+  },
+
+  movieVotesCount: function (movie) {
+    var count = Votes.find({movie: movie}).count();
+
+    if (count) {
+      return " (" + count + "&nbsp;vote" + ((count > 1) ? "s" : "") + ")";
+    } else {
+      return "";
+    }
+  }
+});
+
+Template.buttons.events({
+  'click a': function (e) {
+    e.preventDefault();
+    Votes.insert({ movie: this.title, createdAt: Date.now() });
+  }
+});
