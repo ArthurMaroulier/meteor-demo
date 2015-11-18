f4112bf5bc27be8054deb6b691b792779782ad9d
Arthur Maroulier <git@maroulier.fr>
2015-11-18 21:30:41 +0100

    Add buttons.

--- client/client.js
+++ client/client.js
@@ -2,3 +2,15 @@ Meteor.startup(function () {
   Meteor.subscribe('votes');
   Meteor.subscribe('movies');
 });
+
+Template.buttons.helpers({
+  movies: function () {
+    return Movies.find().fetch();
+  }
+});
+
+Template.button.helpers({
+  getButtonClass: function (str) {
+    return 'btn ' + str.replace(/[^a-z0-9]/ig, '').toLowerCase();
+  }
+});
