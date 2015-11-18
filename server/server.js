168617c76626fa646ffcb8105c093ab43ef30e5e
Arthur Maroulier <git@maroulier.fr>
2015-11-18 21:29:52 +0100

    Add some movies.

--- server/server.js
+++ server/server.js
@@ -6,4 +6,21 @@ Meteor.startup(function () {
   Meteor.publish('movies', function () {
     return Movies.find();
   });
+
+  if (!Movies.find().count()) {
+    [
+      'À la poursuite de demain',
+      'Avengers: L\'ère d\'Ultron',
+      'Jurassic World',
+      'Kingsman : Services secrets',
+      'Mad Max: Fury Road',
+      'Mission: Impossible - Rogue Nation',
+      'Pixels',
+      'Seul sur Mars',
+      'Spectre',
+      'Terminator Genisys'
+    ].forEach(function (d) {
+      Movies.insert({ title: d, createdAt: Date.now()});
+    });
+  }
 });
